7cce5bfdfa367a00ec4bc94f6f3433210b75ee5d
me <me@me.com>
2024-02-22 00:47:11 +0300

    * Создан выделен locators.py из amazon.py

---
+++ amazon/locators.py
@@ -0,0 +1,12 @@
+class Locators:
+    deliver_to = 'xpath', '//div[@id="nav-global-location-slot"]'
+    countries_dropdown = 'xpath', '//select[@id="GLUXCountryList"]'
+    done_button = 'xpath', '//button[@class="a-button-text"]'
+    header = 'tag name', 'header'
+    hamburger_menu = 'xpath', '//*[@id="nav-hamburger-menu"]'
+    menu = 'xpath', '//ul[contains(@class, "hmenu-visible")]//a'
+    hmenu_compressed_btn = 'xpath', '//a[@class = "hmenu-item hmenu-compressed-btn"]'
+    search_results = 'xpath', '//div[contains(@class, "search-result")]//h2/a'
+    title = 'xpath', '//title'
+    add_to_cart = 'xpath', '//input[@id="add-to-cart-button"]'
+    captcha = 'xpath', '//input[@id="captchacharacters"]'
